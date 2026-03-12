cf915fda77d0ba1d71c0d2878432dfa263287a2f
ezaan-ali-30 <ezaanali8310@gmail.com>
2026-03-12 22:37:18 +0530

    ## Day 3:

--- Main.java
+++ Main.java
@@ -1,8 +1,26 @@
-
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        double length = 0;
+        double breadth = 0;
+        double area = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("ENTER LENGTH: ");
+        length = sc.nextDouble();
+
+        System.out.print("ENTER BREADTH: ");
+        breadth = sc.nextDouble();
+
+        area = length * breadth;
+        System.out.println("THE AREA IS: " + area + "cm^2");
+
+        sc.close();
+
+
 
     }
 }
