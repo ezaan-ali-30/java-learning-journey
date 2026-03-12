0b227d595548a2dc6699894b1ba3031d36cbeb6d
ezaan-ali-30 <ezaanali8310@gmail.com>
2026-03-12 23:05:17 +0530

    Create RectangleArea.java with the Rectangle Area program

---
+++ RectangleArea.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class RectangleArea {
+    public static void main(String[] args) {
+        double length = 0;
+        double breadth = 0;
+        double area = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("ENTER LENGTH: ");
+        length = sc.nextDouble();
+
+        System.out.print("ENTER BREADTH: ");
+        breadth = sc.nextDouble();
+
+        area = length * breadth;
+
+        System.out.println("THE AREA IS: " + area + "cm^2");
+
+        sc.close();
+    }
+}
